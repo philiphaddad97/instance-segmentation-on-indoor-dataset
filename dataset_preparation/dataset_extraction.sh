60237116d5757b935783b6c2632313cf1bc2b0a7
Qussai <Mohammad.Jouban@SolutionsUG.onmicrosoft.com>
2022-12-08 20:40:34 +0100

    annotations filtering added

--- dataset_preparation/dataset_extraction.sh
+++ dataset_preparation/dataset_extraction.sh
@@ -9,3 +9,7 @@ rm annotations_trainval2017.zip
 wget http://images.cocodataset.org/zips/val2017.zip
 unzip val2017.zip
 rm val2017.zip
+
+# annotations filtering
+python dataset_filter.py --input_json annotations/instances_train2017.json --output_json annotations/instances_train2017.json --categories chair couch bed 'dining table' window toilet tv oven sink refrigerator
+python dataset_filter.py --input_json annotations/instances_val2017.json --output_json annotations/instances_val2017.json --categories chair couch bed 'dining table' window toilet tv oven sink refrigerator
